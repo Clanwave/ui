6029212b3e3ca01bf4acf55eab9927df860f9c68
Perzeuss <11357019+perzeuss@users.noreply.github.com>
2022-02-09 18:10:52 +0100

    feat(core): make stencil core buildable

--- packages/core/stencil.config.ts
+++ packages/core/stencil.config.ts
@@ -1,6 +1,21 @@
 import { Config } from '@stencil/core';
 
+import { sass } from '@stencil/sass';
+
 export const config: Config = {
   namespace: 'core',
   taskQueue: 'async',
+  plugins: [sass()],
+  outputTargets: [
+    {
+      type: 'dist',
+      esmLoaderPath: '../loader',
+      dir: '../../dist/packages/core/dist',
+    },
+    {
+      type: 'www',
+      dir: '../../dist/packages/core/www',
+      serviceWorker: null, // disable service workers
+    },
+  ],
 };
