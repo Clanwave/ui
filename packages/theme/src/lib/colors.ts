2b057d1e957c53b1afb870969c1c380d2933c929
Perzeuss <11357019+perzeuss@users.noreply.github.com>
2022-02-10 14:59:05 +0100

    feat(theme): create color scheme

---
+++ packages/theme/src/lib/colors.ts
@@ -0,0 +1,15 @@
+// @ts-ignore
+import Color from 'values.js'
+
+export const colors = {
+  primary: new Color('#1c72ff'),
+  secondary: new Color('#7F4DFF'),
+  tertiary: new Color('#7F4DFF'),
+  success: new Color('#7F4DFF'),
+  warning: new Color('#7F4DFF'),
+  danger: new Color('#7F4DFF'),
+  dark: new Color('#040e23'),
+  medium: new Color('#a4adc0'),
+  light: new Color('#ffffff'),
+}
+
